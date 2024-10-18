91f2dfd854a4397f506d82804c8fb0e48ec7481b
komal010 <bhosalekomal369@gmail.com>
2024-10-19 00:31:11 +0530

    To_do_app_basic

---
+++ Flutter/ClassApp/ToDoList/todolist/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/to_do.dart';
+void main(){
+    runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+    const MyApp({super.key});
+
+    @override  
+    Widget build(BuildContext Context){
+        return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: ToDoApp(),
+        );
+    }
+}
---
+++ Flutter/ClassApp/ToDoList/todolist/lib/to_do_modelclass.dart
@@ -0,0 +1,12 @@
+
+class ToDoModelClass {
+  String title;
+  String description;
+  String date;
+
+  ToDoModelClass({
+    required this.title,
+    required this.description,
+    required this.date,
+  });
+}
